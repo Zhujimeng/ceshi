2addd4a6fa7bf59259187462a29ee43d95843b0d
Lenovo <2686269377@qq.com>
2020-04-22 12:53:17 +0800

    搭建Student

---
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -0,0 +1,20 @@
+package cn.jiyun.pojo;
+
+/**
+ * @author shkstart
+ * @user 祝继蒙
+ * @date 2020/4/22 - 12:51
+ */
+public class Student {
+
+    private Integer sid;
+    private String sname;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sid=" + sid +
+                ", sname='" + sname + '\'' +
+                '}';
+    }
+}
