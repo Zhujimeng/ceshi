f95223a79aeff8ab68bccda11fdd9baa5c98a15e
Lenovo <2686269377@qq.com>
2020-04-22 12:55:47 +0800

    生成get set方法

--- src/main/java/cn/jiyun/pojo/Student.java
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -10,6 +10,22 @@ public class Student {
     private Integer sid;
     private String sname;
 
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
